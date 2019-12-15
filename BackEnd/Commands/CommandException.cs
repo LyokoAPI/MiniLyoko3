2a166b923b151c590fe641590109437570dbfe46
GoodOldJack12 <goodoldjack12@gmail.com>
2019-12-15 17:31:47 +0100

    Removed wrapping

--- BackEnd/Commands/CommandException.cs
+++ BackEnd/Commands/CommandException.cs
@@ -13,22 +13,10 @@ namespace Backend.Commands
             Command = command.DisplayName;
         }
 
-        //Extremely Primative Wrap Text
-        private string WrapText(string text)
-        {
-            int i = 0;
-            foreach (var letter in text)
-            {
-                if (i % 73 == 0&&i!=0)
-                    text = text.Insert(i, "\n");
-                i += 1;
-            }
-            return text;
-        }
 
         public override void Resolve(string parameter = "")
         {
-            CommandOutputEvent.Call(Command, WrapText(ErrorMessage));
+            CommandOutputEvent.Call(Command, ErrorMessage);
         }
     }
 }
